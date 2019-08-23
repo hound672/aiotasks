60c11d47245004770856c373ae86696049bbba0d
Vasilii Bliznetcov <hound672@gmail.com>
2019-08-23 21:18:28 +0300

    added convert LTaskInfo

--- aiotasks/_backends/_base.py
+++ aiotasks/_backends/_base.py
@@ -1,10 +1,15 @@
 import asyncio
 from abc import ABC, abstractmethod
 
-from .._structs import LTaskInfo
+from .._structs import LTaskInfo, LTaskStatus
+from .._typing import LTaskUuid
+from .._exceptions import LTaskNotFount
 
 class BaseBackend(ABC):
     """Base class for Backend"""
+
+    class RecordNotFound(Exception):
+        pass
 
     def __init__(self, *,
                  loop: asyncio.AbstractEventLoop,
@@ -21,6 +26,10 @@ class BaseBackend(ABC):
     async def _write(self, key: str, value: dict) -> None:  #pragma: no cover
         pass
 
+    @abstractmethod
+    async def _read(self, key: str) -> dict:  #pragma: no cover
+        pass
+
     async def write_task_info(self, ltask_info: LTaskInfo) -> None:
         """Convert LTaskInfo and write to backend"""
         key = str(ltask_info.uuid)
@@ -28,3 +37,26 @@ class BaseBackend(ABC):
             'status': ltask_info.status.value
         }
         await self._write(key=key, value=value)
+
+    async def read_task_info(self, ltask_uuid: LTaskUuid) -> LTaskInfo:
+        """
+        Read task info from backend
+        Raise LTaskNotFount is ltask was not found
+        """
+        try:
+            data = await self._read(str(ltask_uuid))
+        except BaseBackend.RecordNotFound:
+            raise LTaskNotFount
+
+        if not isinstance(data, dict):
+            raise LTaskNotFount
+
+        try:
+            ltask_info = LTaskInfo(
+                uuid=ltask_uuid,
+                status=LTaskStatus(data['status'])
+            )
+        except (ValueError, KeyError):
+            raise LTaskNotFount
+
+        return ltask_info
--- tests/test_basebeckend.py
+++ tests/test_basebeckend.py
@@ -7,6 +7,7 @@ import pytest
 from aiotasks._backends._base import BaseBackend
 from aiotasks._typing import LTaskUuid
 from aiotasks._structs import LTaskInfo, LTaskStatus
+from aiotasks._exceptions import LTaskNotFount
 
 
 class DummyBackend(BaseBackend):
@@ -17,13 +18,16 @@ class DummyBackend(BaseBackend):
     async def _write(self, key: str, value: dict) -> None:
         pass
 
+    async def _read(self, key: str) -> dict:
+        pass
+
 
 @pytest.fixture
 def dummy_backend():
     return DummyBackend(loop=None, url=None)
 
 
-def test_dump(event_loop, dummy_backend):
+def test_write_task_info(event_loop, dummy_backend):
     ltask_info = LTaskInfo(uuid=LTaskUuid(uuid.uuid4().hex),
                            status=random.choice(list(LTaskStatus)))
 
@@ -34,5 +38,68 @@ def test_dump(event_loop, dummy_backend):
     async def run():
         with patch.object(DummyBackend, '_write', new=_fake):
             await dummy_backend.write_task_info(ltask_info)
+
+    event_loop.run_until_complete(run())
+
+def test_read_task_info_record_not_found(event_loop, dummy_backend):
+    async def _fake(self, key: str):
+        raise BaseBackend.RecordNotFound
+
+    async def run():
+        with patch.object(DummyBackend, '_read', new=_fake):
+            with pytest.raises(LTaskNotFount):
+                await dummy_backend.read_task_info(ltask_uuid=uuid.uuid4().hex)
+
+    event_loop.run_until_complete(run())
+
+
+def test_read_task_info_record_invalid_status(event_loop, dummy_backend, faker):
+    async def _fake(self, key: str):
+        return {'status': faker.word()}
+
+    async def run():
+        with patch.object(DummyBackend, '_read', new=_fake):
+            with pytest.raises(LTaskNotFount):
+                await dummy_backend.read_task_info(ltask_uuid=uuid.uuid4().hex)
+
+    event_loop.run_until_complete(run())
+
+
+def test_read_task_info_record_invalid_struct_data(event_loop, dummy_backend, faker):
+    async def _fake(self, key: str):
+        return {faker.word(): faker.word()}
+
+    async def run():
+        with patch.object(DummyBackend, '_read', new=_fake):
+            with pytest.raises(LTaskNotFount):
+                await dummy_backend.read_task_info(ltask_uuid=uuid.uuid4().hex)
+
+    event_loop.run_until_complete(run())
+
+
+def test_read_task_info_record_invalid_data(event_loop, dummy_backend, faker):
+    async def _fake(self, key: str):
+        return faker.word()
+
+    async def run():
+        with patch.object(DummyBackend, '_read', new=_fake):
+            with pytest.raises(LTaskNotFount):
+                await dummy_backend.read_task_info(ltask_uuid=uuid.uuid4().hex)
+
+    event_loop.run_until_complete(run())
+
+
+def test_read_task_info_record(event_loop, dummy_backend):
+    ltask_info = LTaskInfo(uuid=LTaskUuid(uuid.uuid4().hex),
+                           status=random.choice(list(LTaskStatus)))
+
+    async def _fake(self, key: str):
+        assert key == ltask_info.uuid
+        return {'status': ltask_info.status.value}
+
+    async def run():
+        with patch.object(DummyBackend, '_read', new=_fake):
+            _ltask_info = await dummy_backend.read_task_info(ltask_uuid=ltask_info.uuid)
+            assert ltask_info == _ltask_info
 
     event_loop.run_until_complete(run())
