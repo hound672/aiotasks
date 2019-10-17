13915896c57e81a79329a6e703c860900b8853f5
hound672 <hound672@gmail.com>
2019-10-17 18:22:22 +0300

    another new tests tips

--- tests/test_two.py
+++ tests/test_two.py
@@ -7,10 +7,10 @@ Brief description.
 Some other description
 """
 
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock, Mock
 
 class SomeClass:
-    def logic(self, value, val):
+    def logic(self, value):
         # read from socker, for instance
         return value + '_HELLO'
 
@@ -18,12 +18,35 @@ class SomeClass:
         return self.logic(value)
 
 
-def test_one():
+def Ntest_one():
 
-    with patch.object(SomeClass, 'logic', return_value='DATA', autospec=True) as _mock:
+    # with patch.object(SomeClass, 'logic', return_value='DATA', autospec=True) as _mock:
+    with patch.object(SomeClass, 'logic', autospec=True) as _mock:
         obj = SomeClass()
         res = obj.process('HELLO')
         breakpoint()
 
         # _mock.assert_called()
-        _mock.assert_called_with(obj, 'HELLO')
+        _mock.assert_called_with('HELLO')
+
+
+@patch.object(SomeClass, 'logic', autospec=True)
+def test_two(_mock):
+    obj = SomeClass()
+    res = obj.process('HELLO')
+
+    _mock.assert_called()
+    # _mock.assert_called_with('HELLO')
+
+
+def Ntest_three():
+    obj = SomeClass()
+
+    obj.logic = Mock(spec=obj.logic)
+    obj.logic.return_value = 'DATA'
+
+    res = obj.process('HELLO')
+
+    breakpoint()
+
+    # obj.logic.assert_called_with('HELLO')
